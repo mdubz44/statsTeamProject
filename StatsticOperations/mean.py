9ee9c776d20809a6e92028c3d112c75bb6258169
mdubz44 <matthewwisniewski245@gmail.com>
2020-03-05 18:05:39 -0500

    TASK: Added Mean to calculator

---
+++ StatsticOperations/mean.py
@@ -0,0 +1,5 @@
+class Mean:
+
+    @staticmethod
+    def Mean1(aList):
+        return sum(aList)/len(aList)
---
+++ Tests/test_Statistics.py
@@ -0,0 +1,10 @@
+import unittest
+
+from StatsticOperations import mean
+
+class MyStatsCase(unittest.TestCase):
+
+    def test_StatisticOperations_Mean(self):
+        aList = [1,2,3,4]
+        return mean.Mean.Mean1(aList)
+
