520cf4ae3e04993de2c27bf338fa13193e41637c
mdubz44 <matthewwisniewski245@gmail.com>
2020-03-08 18:08:22 -0400

    TASK: Added Mode to calculator

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -2,6 +2,7 @@ from MathOperations.addition import Addition
 from MathOperations.subtraction import Subtraction
 from StatsticOperations.mean import Mean
 from StatsticOperations.median import Median
+from StatsticOperations.mode import Mode
 
 
 class Calculator:
@@ -18,12 +19,17 @@ class Calculator:
         self.Result = Subtraction.difference(a, b)
         return self.Result
 
-    def Mean(self):
+    def Mean(self, aList):
         aList = [1,2,3,4]
         self.Result = Mean.Mean1(aList)
         return self.Result
 
-    def Median(self):
-        aList = [1,2,3,4]
+    def Median(self, aList):
+        aList = [1,2,2,3,4]
         self.Result = Median.Median1(aList)
+        return self.Result
+
+    def Mode(self, aList):
+        aList = [1,2,3,4]
+        self.Result = Mode.Mode1(aList)
         return self.Result
--- Tests/test_Statistics.py
+++ Tests/test_Statistics.py
@@ -2,6 +2,7 @@ import unittest
 
 from StatsticOperations import mean
 from StatsticOperations import median
+from StatsticOperations import mode
 
 class MyStatsCase(unittest.TestCase):
 
@@ -12,4 +13,8 @@ class MyStatsCase(unittest.TestCase):
     def test_StatsticOperations_Median(self):
         aList = [1,2,3,4]
         return median.Median.Median1(aList)
+
+    def test_StatisticOperations_Mode(self):
+        aList = [1,2,2,3,4]
+        return mode.Mode.Mode1(aList)
 
